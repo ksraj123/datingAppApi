0c15015db750674add75e7e62d4482ec3a1293f2
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-06-17 15:03:28 +0530

    beautify code

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,23 @@ const getUsers = require('./getUsers');
 const likeUser = require('./likeUser');
 require('dotenv').config();
 
+/*
+Routes
+POST    /api/user/login
+POST    /api/user/regiser
+GET     /api/user               - Sends list of users to display for the logged-in user
+PUT     /api/user               - updates DB and emits socket event when user liked
+DELETE  /api/user               - blocks user and updates DB
+GET     /api/user/notifications - Sends list of notifications to frontend
+DELETE  /api/user/notifications - After viewing notifications, requested by frontend
+*/
+
 router.post('/login', login);
-router.post('/register', regFromValidation, register);
-// return to display list of users
 router.get('/', authenticateUser, getUsers);
-// like route
 router.put('/', authenticateUser, likeUser)
 router.delete('/', authenticateUser, blockUser);
+router.post('/register', regFromValidation, register);
 router.get('/notifications', authenticateUser, getNotification);
-// frontend will send delete request after sucessfully fetching and viewing notifications after login 
 router.delete('/notifications', authenticateUser, postNotification);
 
 module.exports = router;
