fbb2c46ededb6683ffff686cca96669e2942e8f1
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-06-17 12:22:44 +0530

    block and get all routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,12 +7,18 @@ const tableName = "users";
 
 // return to display list of users
 router.get('/', authenticateUser, async (req, res)=>{
-    console.log("here!");        
     try{
         let result = await queryDb(`select email, name, imageUrl from ${tableName}`);
+        // list of people who have blocked the user
+        let blockedBy = await queryDb(`select blockedBy from ${tableName} where email='${req.user.email}'`);
+        blockedBy = blockedBy.rows[0].blockedby;
+        result = result.rows;
+        result = result.filter(res => blockedBy.indexOf(res.email) === -1 && res.email !== req.user.email);
+        // console.log(blockedBy);
         res.json({
             email: req.user.email,
-            results: result.rows
+            result,
+            notificationCount: 1 // number of notifications
         })
     } catch (err) {
         console.log(err);
@@ -20,8 +26,29 @@ router.get('/', authenticateUser, async (req, res)=>{
     }
 })
 
-router.put('/like', authenticateUser, (req, res)=>{
-    req.body.email
+// like route
+router.put('/', authenticateUser, async (req, res)=>{
+    // req.body.email - email of user whose image is like
+    try {
+        let result = await queryDb(`select likedBy from ${tableName} where email='${req.body.email}')`);
+
+        await queryDb(`update ${tableName} set likedBy=array_cat(blockedBy, ARRAY['${req.user.email}'])`);
+    } catch(err){
+        res.status(400).send(err);
+    }
+})
+
+router.delete('/', authenticateUser, async (req, res)=>{
+    // req.body.email - email of user to be blocked
+    try{
+        await queryDb(`update ${tableName} set blockedBy=array_cat(blockedBy, ARRAY['${req.user.email}']) where email='${req.body.email}'`);
+        res.json({
+            email: req.body.email,
+            msg: "user blocked"
+        });
+    } catch (err){
+        res.status(400).send(err);
+    }
 })
 
 module.exports.userRouter = router;
